9f801df2ab9541450fe296fccd79dc0727e735b1
Rachel <rachelroegiers@rachels-macbook-pro.local>
2016-03-03 11:35:30 -0800

    Initial Commit

---
+++ Exam 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Exam 1
+//
+//  Created by Rachel Roegiers on 3/3/16.
+//  Copyright © 2016 Rachel Roegiers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
